667bfb25739cf7b5c58f2e301f824e4899492f1b
Luke <lbrobbin@jaguarlandrover.com>
2018-11-18 00:27:11 +0000

    readded leaflet with check for window not being undefined

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
+import L from 'leaflet'
 import styled from 'styled-components'
 import moment from 'moment'
 import croonersPin from '../images/Pin.png'
@@ -19,67 +20,68 @@ class MapComponent extends Component {
   }
 
   createPin = () => {
-    // return L.icon({
-    //   iconUrl: croonersPin,
-    //   iconSize: [38, 30],
-    //   iconAnchor: [16, 29],
-    //   popupAnchor: [-3, -76],
-    // })
+    return L.icon({
+      iconUrl: croonersPin,
+      iconSize: [38, 30],
+      iconAnchor: [16, 29],
+      popupAnchor: [-3, -76],
+    })
   }
 
   render() {
-    return (
-      <p>Map here</p>
-      //   <Map center={[54.375758, -7.352028]} zoom={5.5}>
-      //     <TileLayer
-      //       attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-      //       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      //     />
-      //     {this.props.node.map(node => {
-      //       return (
-      //         <Marker
-      //           position={[node.location.lat, node.location.lon]}
-      //           //   icon={this.createPin()}
-      //         >
-      //           <Popup>
-      //             <strong>{node.theatreName}</strong>
-      //             <p>{moment(node.date).format('Do MMMM YYYY')}</p>
-      //             <a
-      //               href={node.website}
-      //               target="about_blank"
-      //               style={{
-      //                 backgroundColor: 'red',
-      //                 color: 'white',
-      //                 textAlign: 'center',
-      //                 padding: '0.5em 0.75em',
-      //                 maxWidth: '60%',
-      //                 height: '35%',
-      //                 margin: '10px',
-      //                 textDecoration: 'none',
-      //                 border: '1px solid red',
-      //                 textTransform: 'uppercase',
-      //               }}
-      //             >
-      //               Book Now
-      //             </a>
-      //           </Popup>
-      //         </Marker>
-      //       )
-      //     })}
-      //     <MapWrapper>
-      //       <div
-      //         id="map"
-      //         style={{
-      //           height: '600px',
-      //           marginBottom: '6%',
-      //           width: '100vw',
-      //           marginLeft: 'calc(-50vw + 49%)',
-      //         }}
-      //       />
-      //     </MapWrapper>
-      //   </Map>
-      // )
-    )
+    if (typeof window !== 'undefined') {
+      return (
+        <Map center={[54.375758, -7.352028]} zoom={5.5}>
+          <TileLayer
+            attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          {this.props.node.map(node => {
+            return (
+              <Marker
+                position={[node.location.lat, node.location.lon]}
+                icon={this.createPin()}
+              >
+                <Popup>
+                  <strong>{node.theatreName}</strong>
+                  <p>{moment(node.date).format('Do MMMM YYYY')}</p>
+                  <a
+                    href={node.website}
+                    target="about_blank"
+                    style={{
+                      backgroundColor: 'red',
+                      color: 'white',
+                      textAlign: 'center',
+                      padding: '0.5em 0.75em',
+                      maxWidth: '60%',
+                      height: '35%',
+                      margin: '10px',
+                      textDecoration: 'none',
+                      border: '1px solid red',
+                      textTransform: 'uppercase',
+                    }}
+                  >
+                    Book Now
+                  </a>
+                </Popup>
+              </Marker>
+            )
+          })}
+          <MapWrapper>
+            <div
+              id="map"
+              style={{
+                height: '600px',
+                marginBottom: '6%',
+                width: '100vw',
+                marginLeft: 'calc(-50vw + 49%)',
+              }}
+            />
+          </MapWrapper>
+        </Map>
+      )
+    }
+    return null
   }
 }
 
