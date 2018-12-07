1f8d033baf71c92ce9c7af14e03c1056e4b76d15
Luke <lbrobbin@jaguarlandrover.com>
2018-12-07 22:42:36 +0000

    Added Gallery component

---
+++ src/components/Carousel.js
@@ -0,0 +1,90 @@
+import React, { Component } from 'react'
+import styled from 'styled-components'
+import Slider from 'react-slick'
+
+const SliderWrapper = styled.div`
+  padding: 2% 5% 2%;
+  width: 80%;
+  margin: auto;
+  @media screen and (max-width: 500px) {
+    width: 100%;
+    padding: 2% 0% 2%;
+  }
+  .Slider-inner-image {
+    border: 3px solid #f7b300;
+    width: 90%;
+    margin: auto;
+  }
+  .slick-dots li.slick-active button:before {
+    color: #f7b300;
+  }
+  .slick-dots li button:hover:before {
+    color: white;
+  }
+  .slick-dots li button:focus:before {
+    color: white;
+  }
+
+  .Slider-inner {
+    padding-bottom: 2%;
+  }
+  .Slider-inner-bottom {
+    padding: 1%;
+    @media screen and (max-width: 500px) {
+      visibility: hidden;
+    }
+  }
+`
+
+class Gallery extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      nav1: null,
+      nav2: null,
+    }
+  }
+
+  componentDidMount() {
+    this.setState({
+      nav1: this.slider1,
+      nav2: this.slider2,
+    })
+  }
+  render() {
+    const responsive = [
+      {
+        breakpoint: 1024,
+        settings: {
+          arrows: false,
+          dots: true,
+        },
+      },
+    ]
+    return (
+      <SliderWrapper>
+        <Slider
+          asNavFor={this.state.nav2}
+          ref={slider => (this.slider1 = slider)}
+          className="Slider-inner"
+          responsive={responsive}
+        >
+          {this.props.content}
+        </Slider>
+        <Slider
+          asNavFor={this.state.nav1}
+          ref={slider => (this.slider2 = slider)}
+          slidesToShow={5}
+          swipeToSlide={true}
+          focusOnSelect={true}
+          className="Slider-inner-bottom"
+          responsive={responsive}
+        >
+          {this.props.content}
+        </Slider>
+      </SliderWrapper>
+    )
+  }
+}
+
+export default Gallery
--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, { Component } from 'react'
 import styled from 'styled-components'
+import Carousel from '../components/Carousel'
+import { StaticQuery, graphql } from 'gatsby'
 import Header from '../components/header'
 import CroonersLogo from '../components/ImageComponents/CroonersLogo'
 import Layout from '../components/layout'
@@ -15,18 +17,79 @@ const Logo = styled.div`
     width: 80%;
   }
 `
-const GalleryWrapper = styled.div`
-  width: 100%;
+
+const Title = styled.h2`
+  padding-top: 0.5em;
+  margin: auto;
+  text-align: center;
 `
-const Gallery = () => (
-  <Layout>
-    <Header page="Gallery" />
-    <GalleryWrapper>
-      <Logo>
-        <CroonersLogo />
-      </Logo>
-    </GalleryWrapper>
-  </Layout>
-)
+
+const GalleryWrapper = styled.div`
+  @media screen and (max-width: 1166px) {
+    width: 90vw;
+  }
+`
+class Gallery extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      nav1: null,
+      nav2: null,
+    }
+  }
+
+  componentDidMount() {
+    this.setState({
+      nav1: this.slider1,
+      nav2: this.slider2,
+    })
+  }
+  render() {
+    return (
+      <StaticQuery
+        query={graphql`
+          query {
+            allContentfulGalleryImages {
+              edges {
+                node {
+                  image {
+                    fluid(maxWidth: 1200) {
+                      src
+                    }
+                  }
+                }
+              }
+            }
+          }
+        `}
+        render={({ allContentfulGalleryImages: { edges } }) => {
+          const content = edges[0].node.image.map(singleImage => (
+            <div key={singleImage.fluid.src}>
+              <img
+                className="Slider-inner-image"
+                src={singleImage.fluid.src}
+                alt="Croners on stage"
+              />
+            </div>
+          ))
+          return (
+            <Layout>
+              <Header page="Gallery" />
+              <Logo>
+                <CroonersLogo />
+              </Logo>
+              <GalleryWrapper>
+                <Title>Production Photos</Title>
+                <Carousel content={content} />
+                <Title>videos</Title>
+                <Carousel content={content} />
+              </GalleryWrapper>
+            </Layout>
+          )
+        }}
+      />
+    )
+  }
+}
 
 export default Gallery
